ee2fbb8fefe7f590a3be3cd3bb415595119a0684
Narek <narekbayanduryan16@gmail.com>
2023-05-10 20:55:02 +0300

    Creating a home page.

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name = "catalog"
+# app_name = "catalog"
 urlpatterns = [
-	
+	path('', views.index, name="index"),	
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+	"""Функция отображения для домашней страницы сайта."""
+	
+	# Генерация "количеств" некоторых главных обьектов.
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+
+	# Доступные книги (статус = "а")
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+	num_authors = Author.objects.count()
+
+
+	context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors}
+
+	return render(request, 'index.html', context)
