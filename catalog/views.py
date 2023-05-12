9f5ffa37e97d7d4ace35d652c52751fc4bb161ba
Narek <narekbayanduryan16@gmail.com>
2023-05-12 17:56:46 +0300

    Now the main template can show the number of visits to the user.

--- catalog/views.py
+++ catalog/views.py
@@ -14,8 +14,12 @@ def index(request):
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 	num_authors = Author.objects.count()
 
+	# Количество посещений этого представления, подсчитанное в переменной сеанса.
+	num_visits = request.session.get('num_visits', 0)
+	request.session['num_visits'] = num_visits + 1
 
-	context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors}
+
+	context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_visits': num_visits}
 
 	return render(request, 'index.html', context)
 
